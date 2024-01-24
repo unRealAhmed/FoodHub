6013115e922028fd64793872247c821cd0c3d2ae
Ahmed Sayed <therealahme@gmail.com>
2024-01-24 16:27:07 +0200

    build create-restaurant dto

--- src/api/restaurants/dtos/create-restaurant.dto.ts
+++ src/api/restaurants/dtos/create-restaurant.dto.ts
@@ -3,15 +3,15 @@ import { IsString, IsNumber, IsNotEmpty, Min, Max } from 'class-validator';
 export class CreateRestaurantDto {
   @IsNotEmpty()
   @IsString()
-  name: string;
+  name!: string;
 
   @IsNotEmpty()
   @IsNumber()
   @Min(1)
   @Max(5)
-  rating: number;
+  rating!: number;
 
   @IsNotEmpty()
   @IsString()
-  location: string;
+  location!: string;
 }
