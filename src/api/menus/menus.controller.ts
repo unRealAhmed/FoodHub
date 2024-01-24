750fe7b600999a6a828e42a60c5d30383962634e
Ahmed Sayed <therealahme@gmail.com>
2024-01-24 16:32:48 +0200

    create menu module

--- src/api/menus/menus.controller.ts
+++ src/api/menus/menus.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+  Patch,
+} from '@nestjs/common';
+import { MenusService } from './menus.service';
+import { CreateMenuDto } from './dtos/create-menu.dto';
+import { UpdateMenuDto } from './dtos/update-menu.dto';
+import { Menu } from './menus.entity';
 
 @Controller('menus')
-export class MenusController {}
+export class MenusController {
+  constructor(private readonly menusService: MenusService) {}
+
+  @Get()
+  async getAllMenus(): Promise<Menu[]> {
+    return this.menusService.getAllMenus();
+  }
+
+  @Get(':id')
+  async getMenuById(@Param('id') id: number): Promise<Menu> {
+    return this.menusService.getMenuById(id);
+  }
+
+  @Post()
+  async createMenu(@Body() createMenuDto: CreateMenuDto): Promise<Menu> {
+    return this.menusService.createMenu(createMenuDto);
+  }
+
+  @Patch(':id')
+  async updateMenu(
+    @Param('id') id: number,
+    @Body() updateMenuDto: UpdateMenuDto,
+  ): Promise<Menu> {
+    return this.menusService.updateMenu(id, updateMenuDto);
+  }
+
+  @Delete(':id')
+  async deleteMenu(@Param('id') id: number): Promise<void> {
+    return this.menusService.deleteMenu(id);
+  }
+}
