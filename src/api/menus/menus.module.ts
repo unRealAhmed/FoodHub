03238e7bf9d505048316e75c263837dab7ecd94e
Ahmed Sayed <therealahme@gmail.com>
2024-01-24 16:33:30 +0200

    create menus module

--- src/api/menus/menus.module.ts
+++ src/api/menus/menus.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MenusController } from './menus.controller';
 import { MenusService } from './menus.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Menu } from './menus.entity';
+import { MenusRepository } from './menus.repository';
+import { RestaurantsModule } from '../restaurants/restaurants.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Menu]), RestaurantsModule],
   controllers: [MenusController],
-  providers: [MenusService]
+  providers: [MenusService, MenusRepository],
 })
 export class MenusModule {}
