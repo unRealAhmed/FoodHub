8b19609fa1beee38634131ca9a33724035b670a1
Ahmed Sayed <therealahme@gmail.com>
2024-01-27 00:40:41 +0200

    create item entity

---
+++ src/api/items/items.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Item {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  price!: number;
+}
