35faef2fd9d7ca27a883a32b17a5745950eedd6c
Ahmed Sayed <therealahme@gmail.com>
2024-01-27 00:39:41 +0200

    create category service

--- src/api/categories/categories.service.ts
+++ src/api/categories/categories.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Category } from './categories.entity';
+import { CreateCategoryDto } from './dtos/create-category.dto';
+import { CATEGORY_NOT_FOUND } from 'src/assets/messages';
+import { CategoryRepository } from './categories.repository';
 
 @Injectable()
-export class CateogriesService {}
+export class CategoryService {
+  constructor(private readonly categoryRepository: CategoryRepository) {}
+
+  async createCategory(category: CreateCategoryDto): Promise<Category> {
+    console.log(category);
+    return this.categoryRepository.createCategory(category);
+  }
+
+  async getAllCategories(): Promise<Category[]> {
+    return this.categoryRepository.getAllCategories();
+  }
+
+  async getCategoryByName(name: string): Promise<Category | null> {
+    return this.categoryRepository.getCategoryByName(name);
+  }
+
+  async getCategoryById(id: number): Promise<Category | null> {
+    return this.categoryRepository.findOne({ where: { id } });
+  }
+
+  async updateCategory(
+    id: number,
+    updateCategoryDto: CreateCategoryDto,
+  ): Promise<Category> {
+    const updatedCateogry = await this.categoryRepository.updateCategory(
+      id,
+      updateCategoryDto,
+    );
+
+    if (!updatedCateogry) {
+      throw new NotFoundException(CATEGORY_NOT_FOUND(id));
+    }
+    return updatedCateogry;
+  }
+
+  async deleteCategory(id: number): Promise<void> {
+    await this.categoryRepository.deleteCategory(id);
+  }
+}
