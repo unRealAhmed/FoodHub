5ddd6db393dfeb4c829978ba2e39d11d5b6bad99
Ahmed Sayed <therealahme@gmail.com>
2024-01-27 00:42:24 +0200

    finished items module

--- src/api/items/items.module.ts
+++ src/api/items/items.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Item } from './items.entity';
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
+import { ItemsRepository } from './items.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Item])],
   controllers: [ItemsController],
-  providers: [ItemsService]
+  providers: [ItemsService, ItemsRepository],
 })
-export class ItemsModule {}
+export class ItemModule {}
