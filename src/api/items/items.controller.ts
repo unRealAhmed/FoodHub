7bc032a982c0ff1659bfc3413b6e895607b57d02
Ahmed Sayed <therealahme@gmail.com>
2024-01-27 00:42:05 +0200

    create items controller

--- src/api/items/items.controller.ts
+++ src/api/items/items.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Put,
+  Post,
+} from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { Item } from './items.entity';
+import { UpdateItemDto } from './dtos/update-item.dto';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  @Post()
+  async createItem(@Body() createItemDto: UpdateItemDto): Promise<Item> {
+    return this.itemsService.createItem(createItemDto);
+  }
+
+  @Get()
+  async getAllItems(): Promise<Item[]> {
+    return this.itemsService.getAllItems();
+  }
+
+  @Get(':name')
+  async getItemByName(@Param('name') name: string): Promise<Item | null> {
+    return this.itemsService.getItemByName(name);
+  }
+
+  @Put(':id')
+  async updateItem(
+    @Param('id') id: number,
+    @Body() updateItemDto: UpdateItemDto,
+  ): Promise<Item> {
+    return this.itemsService.updateItem(id, updateItemDto);
+  }
+
+  @Delete(':id')
+  async deleteItem(@Param('id') id: number): Promise<void> {
+    return this.itemsService.deleteItem(id);
+  }
+}
