ca67854ea8ebdf76a021f0cf34ef9d5f2410a91c
Ahmed Sayed <therealahme@gmail.com>
2024-01-27 00:44:13 +0200

    add category and items messages

--- src/assets/messages.ts
+++ src/assets/messages.ts
@@ -2,3 +2,12 @@ export const RESTAURANT_NOT_FOUND = (id: number) =>
   `Restaurant with id ${id} not found`;
 
 export const MENU_NOT_FOUND = (id: number) => `Menu with id ${id} not found`;
+
+export const CATEGORY_NOT_FOUND = (id: number) =>
+  `Category with id ${id} not found`;
+
+export const ITEM_NOT_FOUND = (name: string) =>
+  `Item with name ${name} not found`;
+
+export const ITEM_NOT_FOUND_ID = (id: number) =>
+  `Item with name ${id} not found`;
