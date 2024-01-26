4dc3f74d68bd4612740d7258bf81345ec532efde
Ahmed Sayed <therealahme@gmail.com>
2024-01-27 00:42:57 +0200

    create category-item entity as junction table

---
+++ src/api/category-item/category-item.entity.ts
@@ -0,0 +1,18 @@
+// category-item.entity.ts
+import { Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { Category } from '../categories/categories.entity';
+import { Item } from '../items/items.entity';
+
+@Entity()
+export class CategoryItem {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @ManyToOne(() => Category, { eager: true })
+  @JoinColumn({ name: 'category_id' })
+  category!: Category;
+
+  @ManyToOne(() => Item, { eager: true })
+  @JoinColumn({ name: 'item_id' })
+  item!: Item;
+}
