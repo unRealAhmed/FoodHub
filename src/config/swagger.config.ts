4e23550a8bb0b9442ddb95e92c30030623546643
Ahmed Sayed <therealahme@gmail.com>
2024-01-27 16:33:34 +0200

    configure swagger

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,19 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication) {
+  const options = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('Your API Description')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/', 'Local environment')
+    .addTag('Restaurants', 'Endpoints related to restaurants')
+    .addTag('Menus', 'Endpoints related to menus')
+    .addTag('Categories', 'Endpoints related to categories')
+    .addTag('Items', 'Endpoints related to items')
+    .addTag('CategoryItem', 'Endpoints related to category items')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,8 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  setupSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
