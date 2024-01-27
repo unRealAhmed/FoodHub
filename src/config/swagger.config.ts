9283228319f930d57837cb77bbde60f00d6eb24b
Ahmed Sayed <therealahme@gmail.com>
2024-01-27 17:10:15 +0200

    edit title and description

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -3,15 +3,16 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 export function setupSwagger(app: INestApplication) {
   const options = new DocumentBuilder()
-    .setTitle('Your API Title')
-    .setDescription('Your API Description')
+    .setTitle('FoodHub API Documentation')
+    .setDescription(
+      'API for managing restaurants, menus, items, categories, and their associations.',
+    )
     .setVersion('1.0')
-    .addServer('http://localhost:3000/', 'Local environment')
     .addTag('Restaurants', 'Endpoints related to restaurants')
     .addTag('Menus', 'Endpoints related to menus')
     .addTag('Categories', 'Endpoints related to categories')
     .addTag('Items', 'Endpoints related to items')
-    .addTag('CategoryItem', 'Endpoints related to category items')
+    .addTag('Category Items', 'Endpoints related to category items')
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
