7a1b8a56f903d82ea67f282110085eb6adfc9af8
Ahmed Sayed <therealahme@gmail.com>
2024-02-10 16:41:49 +0200

    add swager docs and refactor some code

--- src/api/users/user.controller.ts
+++ src/api/users/user.controller.ts
@@ -3,23 +3,84 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { User } from './user.entity';
 
+import {
+  ApiTags,
+  ApiBody,
+  ApiOkResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiBadRequestResponse,
+  ApiOperation,
+  ApiParam,
+} from '@nestjs/swagger';
+import {
+  LIST_ALL_USERS,
+  USER_CREATED_SUCCESSFULLY,
+  USER_UPDATED_SUCCESSFULLY,
+} from 'src/common/assets/messages';
+
 @Controller('users')
+@ApiTags('Users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
   @Post('signup')
-  async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
-    return await this.userService.createUser(createUserDto);
+  @ApiOperation({
+    summary: 'Create a new user',
+    description: 'create a new user.',
+  })
+  @ApiBody({ type: CreateUserDto })
+  @ApiCreatedResponse({
+    description: 'User created successfully.',
+    type: User,
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad request. Check the request body.',
+  })
+  async createUser(
+    @Body() createUserDto: CreateUserDto,
+  ): Promise<{ message: string; data: User }> {
+    const user = await this.userService.createUser(createUserDto);
+    return { message: USER_CREATED_SUCCESSFULLY, data: user };
   }
+
   @Get()
-  async getAllUsers(): Promise<User[]> {
-    return await this.userService.getAllUsers();
+  @ApiOperation({
+    summary: 'Get all users',
+    description: 'retrieve a list of all users.',
+  })
+  @ApiOkResponse({
+    description: 'List of all users.',
+    type: User,
+    isArray: true,
+  })
+  async getAllUsers(): Promise<{ message: string; data: User[] }> {
+    const users = await this.userService.getAllUsers();
+    return { message: LIST_ALL_USERS, data: users };
   }
 
   @Put(':id')
-  async updateRestaurant(
+  @ApiOperation({
+    summary: 'Update a user by ID',
+    description: 'update a user by its ID.',
+  })
+  @ApiBody({ type: CreateUserDto })
+  @ApiOkResponse({
+    description: 'User updated successfully.',
+    type: User,
+  })
+  @ApiNotFoundResponse({
+    description: 'User not found.',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'The ID of the user.',
+  })
+  async updateUser(
     @Param('id') id: number,
     @Body() updateUserDto: CreateUserDto,
-  ): Promise<User> {
-    return this.userService.updateUser(id, updateUserDto);
+  ): Promise<{ message: string; data: User }> {
+    const user = await this.userService.updateUser(id, updateUserDto);
+    return { message: USER_UPDATED_SUCCESSFULLY, data: user };
   }
 }
