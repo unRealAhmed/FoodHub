c4a395024e90ef6c934491aa0418b2f8b916bce6
Ahmed Sayed <therealahme@gmail.com>
2024-02-10 16:09:54 +0200

    add restaurant messages

--- src/common/assets/messages.ts
+++ src/common/assets/messages.ts
@@ -1,3 +1,4 @@
+//NOT_FOUND MESSAGES
 export const RESTAURANT_NOT_FOUND = (id: number) =>
   `Restaurant with id ${id} not found`;
 
@@ -12,6 +13,17 @@ export const ITEM_NOT_FOUND = (name: string) =>
 export const ITEM_NOT_FOUND_ID = (id: number) =>
   `Item with name ${id} not found`;
 
+//RESTAURANT MESSAGES
+export const RESTAURANT_CREATED_SUCCESSFULLY =
+  'Restaurant created successfully.';
+export const LIST_OF_ALL_RESTAURANTS = 'List of all restaurants.';
+export const RESTAURANT_FOUND = 'Restaurant found.';
+export const RESTAURANT_UPDATED_SUCCESSFULLY =
+  'Restaurant updated successfully.';
+export const RESTAURANT_DELETED_SUCCESSFULLY =
+  'Restaurant deleted successfully.';
+
+//ORDER MESSAGES
 export const ORDER_FULFILLED = 'fulfilled.';
 export const ORDER_IN_PROGRESS = 'inProgress';
 export const ORDER_CANCELLED_BY_USER = 'cancelledByUser';
